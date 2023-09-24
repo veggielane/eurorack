319c2a33511e1a0f7dcb7c884b3742f5e97c9e7b
veggielane <chris@lane.org.uk>
2023-09-24 13:07:21 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1,38 +1,19 @@
-from eurorack.panels import Panel, PanelSize
-from typing import List
 import cadquery as cq
 
-from eurorack.part import Component
+# The dimensions of the box. These can be modified rather than changing the
+# object's code directly.
+length = 80.0
+height = 60.0
+thickness = 10.0
+center_hole_dia = 22.0
 
+# Create a box based on the dimensions above and add a 22mm center hole
+result = (
+    cq.Workplane("XY")
+    .box(length, height, thickness)
+    .faces(">Z")
+    .workplane()
+    .hole(center_hole_dia)
+)
 
-def output(components: List[Component]):
-    for comp in components:
-        print(f"building {comp.name}")
-        built = comp.build().val()
-        built.exportStep(f"outputs/{comp.number}.step")
-        cq.exporters.export(built, f"outputs/{comp.number}.stl")
-
-
-def build(components: List[Component]):
-    return dict(zip(components, map(lambda comp: comp.build().val(), components)))
-
-
-parts = [
-    Panel(PanelSize.HP_1),
-    Panel(PanelSize.HP_1_5),
-    Panel(PanelSize.HP_2),
-    Panel(PanelSize.HP_3),
-    Panel(PanelSize.HP_4),
-    Panel(PanelSize.HP_6),
-    Panel(PanelSize.HP_8),
-    Panel(PanelSize.HP_10),
-    Panel(PanelSize.HP_12),
-    Panel(PanelSize.HP_14),
-    Panel(PanelSize.HP_16),
-    Panel(PanelSize.HP_18),
-    Panel(PanelSize.HP_22),
-    Panel(PanelSize.HP_28),
-    Panel(PanelSize.HP_42),
-]
-
-output(parts)
+cq.exporters.export(result, f"outputs/test.stl")
